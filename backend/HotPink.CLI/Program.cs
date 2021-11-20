811b060de1e073259d881462355a571ff45ed613
Jan Skála <j.skala@hotmail.com>
2021-11-20 21:53:44 +0100

    Add tool to delete old data.

---
+++ backend/HotPink.CLI/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hl7.Fhir.Rest;
+
+using HotPink.API;
+using HotPink.API.Services;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var services = new ServiceCollection();
+
+services.AddSingleton<IConfiguration>(configuration);
+
+// business logic
+services.AddSingleton<InvitationService>();
+services.AddSingleton<PatientService>();
+services.AddHttpClient<ClassificationService>();
+
+// FHIR
+services.AddSingleton(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var key = configuration["FhirApiKey"];
+    var client = new FhirClient("https://fhir.afuwmxvolwu6.static-test-account.isccloud.io", messageHandler: new ApiKeyMessageHandler(key));
+    client.Settings.PreferredFormat = ResourceFormat.Json;
+    return client;
+});
+
+var di = services.BuildServiceProvider();
+
+var fhir = di.GetRequiredService<FhirClient>();
+var patientServices = di.GetRequiredService<PatientService>();
+
+var detail = await patientServices.GetPatientDetail("1");
+
+foreach (var record in detail.Data.Skip(5))
+{
+    Console.WriteLine("Deleting: " + record.Id);
+    await fhir.DeleteAsync($"Observation/{record.Id}");
+    Console.WriteLine("Deleted: " + record.Id);
+}
