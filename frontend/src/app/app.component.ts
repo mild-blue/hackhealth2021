d3e85c696f3864f87898453dbc267041bc0c9bf4
Anastasia <surikova.anastasia@gmail.com>
2021-11-20 01:00:29 +0100

    not working

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { MediaCapture } from '@ionic-native/media-capture/ngx';
-import { Flashlight } from '@ionic-native/flashlight/ngx';
-import { CameraPreview, CameraPreviewOptions, CameraPreviewPictureOptions } from '@ionic-native/camera-preview/ngx';
+import { CaptureError, MediaCapture, MediaFile } from '@ionic-native/media-capture/ngx';
+import { AlertController, Platform } from '@ionic/angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { File, FileEntry } from '@ionic-native/file/ngx';
+import { HttpClient } from '@angular/common/http';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+
+// import { Filesystem } from '@capacitor/filesystem';
+
+interface VideoCapture {
+  name: string;
+  localURL: string;
+  type: string;
+  lastModified?: string;
+  lastModifiedDate?: number;
+  size: number;
+  start: number;
+  end: number;
+  fullPath: string;
+}
+
+const MEDIA_FOLDER_NAME = 'my_media';
 
 @Component({
   selector: 'app-root',
@@ -10,86 +29,158 @@ import { CameraPreview, CameraPreviewOptions, CameraPreviewPictureOptions } from
 })
 export class AppComponent implements OnInit {
   constructor(private mediaCapture: MediaCapture,
-              private flashlight: Flashlight,
-              private cameraPreview: CameraPreview) {
+              private alertService: AlertController,
+              private file: File,
+              private sanitizer: DomSanitizer,
+              private plt: Platform,
+              private http: HttpClient,
+              private webview: WebView) {
   }
 
   ngOnInit() {
-    this.init2();
-  }
+    this.plt.ready().then(() => {
+      const path = this.file.dataDirectory;
+      this.file.checkDir(path, MEDIA_FOLDER_NAME).then(
+        () => {
+          console.log('Dir exists');
+          this.init();
+        },
+        err => {
+          this.file.createDir(path, MEDIA_FOLDER_NAME, false).then(() => {
+            this.init();
+          });
 
-  async init2() {
-    const cameraPreviewOpts: CameraPreviewOptions = {
-      x: 25,
-      y: 25,
-      width: window.screen.width - 50,
-      height: window.screen.height - 50,
-      camera: 'rear',
-      tapPhoto: true,
-      previewDrag: true,
-      toBack: true,
-      alpha: 1
-    };
-
-    const opts = {
-      cameraDirection: this.cameraPreview.CAMERA_DIRECTION.BACK,
-      width: (window.screen.width / 2),
-      height: (window.screen.height / 2),
-      quality: 60,
-      withFlash: true
-    };
-
-    // await this.cameraPreview.setFlashMode(this.cameraPreview.FLASH_MODE.ON);
-    // this.cameraPreview.startRecordVideo(opts).then(filePath => console.log(filePath));
-    const pictureOpts: CameraPreviewPictureOptions = {
-      width: 1280,
-      height: 1280,
-      quality: 85
-    };
-
-    this.cameraPreview.startCamera(cameraPreviewOpts).then(
-      async (res) => {
-        this.cameraPreview.setFlashMode(this.cameraPreview.FLASH_MODE.TORCH);
-        this.cameraPreview.startRecordVideo(opts);
-        // todo: use if video recording doesnt work
-        // const base64s = [];
-        // for (let i = 1; i <= 300; i++) {
-        //   const image = await this.cameraPreview.takeSnapshot(pictureOpts);
-        //   base64s.push(image);
-        // }
-
-
-        console.log(res);
-      },
-      (err) => {
-        console.log(err);
-      });
+        }
+      );
+    });
 
 
   }
 
   async init() {
-    // const options: CaptureImageOptions = { limit: 3 };
-    // this.mediaCapture.captureImage(options)
-    // .then(
-    //   (data: MediaFile[]) => console.log(data),
-    //   (err: CaptureError) => console.error(err)
-    // );
+    const alert = await this.alertService.create({
+      message: `<h3>Please turn on your flashlight</h3>`,
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel',
+          handler: value => {
+            this.captureVideo();
+          }
+        }
+      ]
+    });
 
+    return alert.present();
+  }
 
+  async captureVideo() {
     try {
-      this.mediaCapture.captureVideo({ duration: 5 }).then(video => console.log('VIDEO', video));
-      this.mediaCapture.onPendingCaptureResult().subscribe(result => {
-        console.log('RESULT', result);
-      });
-      await this.flashlight.switchOn();
+      const result: MediaFile[] | CaptureError = await this.mediaCapture.captureVideo({ duration: 5 });
+      if ('code' in result) {
+        return;
+      }
+
+      const file = (result as MediaFile[])[0];
+      const video = file as unknown as VideoCapture;
+      console.log('video', video);
+
+      const fileEntry: FileEntry = await this.copyFileToLocalDir(video.fullPath);
+      console.log('fileEntry', fileEntry);
+
+      const src = this.sanitizer.bypassSecurityTrustResourceUrl(fileEntry.nativeURL);
+      console.log('src', src);
+
+      // fileEntry.file(async (f) => {
+      //   console.log(f);
+      //   const ab = await f.arrayBuffer();
+      //   console.log(ab);
+      // });
+
+      // const a = this.webview.convertFileSrc(fileEntry.nativeURL);
+      // console.log(a);
+
+      // const b = await this.uriToBase64(fileEntry.nativeURL);
+      // const promise = this.file.readAsText(this.file.dataDirectory + MEDIA_FOLDER_NAME, fileEntry.name);
+      // await promise.then(value => {
+      //   console.log(value);
+      // });
+      const path = 'file:/' + video.fullPath.substring(0, video.fullPath.lastIndexOf('/'));
+      console.log(path, video.name);
+      this.file.readAsArrayBuffer(path, video.name).then(
+        r => console.log(r),
+        error => {
+          console.log('error', error);
+        }
+      ).catch(e => console.log(e));
+
+      // fetch(video.localURL).then(res => console.log(res)).catch(e => console.log(e));
+      // fetch(fileEntry.nativeURL).then(res => console.log(res)).catch(e => console.log(e));
+      // const data: Observable<Blob> = this.http.get<Blob>(a, { headers: { responseType: 'blob' } });
+      // data.subscribe((response: Blob) => {
+      //   console.log(response);
+      // }, (error: HttpErrorResponse) => {
+      //   console.log(error.error.text);
+      // });
+
+      // const contents = await Filesystem.readFile({
+      //   path: fileEntry.nativeURL
+      // });
+      //
+      // console.log('data:', contents);
+
+      // const reader = new FileReader();
+      // reader.onload = (e) => {
+      //   // binary data
+      //   console.log(e.target.result);
+      // };
+      // reader.onerror = (e) => {
+      //   // error occurred
+      //   console.log('Error : ' + e.type);
+      // };
+      // reader.readAsBinaryString(file.fullPath);
     } catch (e) {
       console.log('ERROR', e);
     } finally {
+    }
+  }
 
-      this.flashlight.switchOff();
+  uriToBase64(uri) {
+    let nameFile = uri.replace(this.file.dataDirectory, '');
+
+    return this.file.readAsDataURL(this.file.dataDirectory, nameFile).then((file64) => {
+      let base64String = ('' + file64 + '').replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
+      console.log('data: ', base64String); // Receiving the base 64 correctly here
+      return base64String;
+    })
+    .catch(err => {
+      console.log('Error while transforming image to base64: ', err);
+      return err;
+    });
+  }
+
+  async copyFileToLocalDir(fullPath): Promise<FileEntry | undefined> {
+    let myPath = fullPath;
+    // Make sure we copy from the right location
+    if (fullPath.indexOf('file://') < 0) {
+      myPath = 'file://' + fullPath;
     }
 
+    const ext = myPath.split('.').pop();
+    const d = Date.now();
+    const newName = `${d}.${ext}`;
 
+    const name = myPath.substr(myPath.lastIndexOf('/') + 1);
+    const copyFrom = myPath.substr(0, myPath.lastIndexOf('/') + 1);
+    const copyTo = this.file.dataDirectory + MEDIA_FOLDER_NAME;
+
+
+    try {
+      const fileEntry = await this.file.copyFile(copyFrom, name, copyTo, newName);
+
+      return fileEntry as FileEntry;
+    } catch (error) {
+      console.log('error: ', error);
+    }
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,17 +7,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaCapture } from '@ionic-native/media-capture/ngx';
-import { Flashlight } from '@ionic-native/flashlight/ngx';
-import { CameraPreview } from '@ionic-native/camera-preview/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { HttpClientModule } from '@angular/common/http';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     MediaCapture,
-    Flashlight,
-    CameraPreview,
+    File,
+    WebView,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
