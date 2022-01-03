0c59d29ed1a4ff4db699f4169952b8ea217dd2c5
Jan Skála <j.skala@hotmail.com>
2022-01-03 15:20:20 +0100

    Set CORS to *.

--- backend/HotPink.API/Program.cs
+++ backend/HotPink.API/Program.cs
@@ -37,12 +37,6 @@ builder.Services.AddSingleton(provider =>
     return client;
 });
 
-// CORS
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(builder => builder.WithOrigins("*"));
-});
-
 // JSON
 builder.Services.AddControllers().AddJsonOptions(x =>
 {
@@ -51,6 +45,9 @@ builder.Services.AddControllers().AddJsonOptions(x =>
 });
 
 var app = builder.Build();
+
+// CORS
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 // Swagger
 app.UseSwagger();
